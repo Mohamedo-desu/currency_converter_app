063b8fc2e91ce6a80a6ee0abeda89537ed7f2e97
Mohamedo-desu <omkahub@gmail.com>
2025-09-03 20:23:46 +0300

    feat: update app version to 8.0.0 in configuration files and improve alert handling in useVersion hook

--- src/hooks/useVersion.ts
+++ src/hooks/useVersion.ts
@@ -243,7 +243,7 @@ export const useVersion = () => {
                     onPress: handleDownloadAndInstall,
                   },
                 ],
-                { cancelable: true } //TODO
+                { cancelable: true }
               );
             }
 
